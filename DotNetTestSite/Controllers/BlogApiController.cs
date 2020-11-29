e334dae053121c8b88a2190ea60e6c71a6360d0c
Declan <dwright1991@gmail.com>
2020-11-29 18:36:44 +0000

    Added blog page

---
+++ DotNetTestSite/Controllers/BlogApiController.cs
@@ -0,0 +1,53 @@
+﻿using DotNetTestSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace DotNetTestSite.Controllers
+{
+    [ApiController]
+    public class BlogApiController : ControllerBase
+    {
+        [HttpGet("blog/all")]
+        public ActionResult<string> GetAllBlogPosts()
+        {
+            var url = "https://www.blogger.com/feeds/4886411145150219450/posts/default";
+            List<BlogPost> blogPosts = new List<BlogPost>();
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml"));
+                HttpResponseMessage response = client.GetAsync(url).Result;
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    XDocument xdoc = XDocument.Parse(response.Content.ReadAsStringAsync().Result);
+                    var posts = xdoc.Descendants();
+                    var test = posts.Where(n => n.Name.ToString().Contains("entry"));
+                    foreach (XElement n in test)
+                    {
+                        var children = n.Elements();
+                        if(children != null)
+                        {
+                            var title = children.FirstOrDefault(n => n.Name.ToString().Contains("title")).Value;
+                            var content = children.FirstOrDefault(n => n.Name.ToString().Contains("content")).Value;
+                            var published = children.FirstOrDefault(n => n.Name.ToString().Contains("published")).Value;
+                            blogPosts.Add(new BlogPost { Title = title, Content = content, Published = DateTime.Parse(published) });
+                        }
+                    }
+                }
+
+            }
+            var resp = Newtonsoft.Json.JsonConvert.SerializeObject(blogPosts);
+            return Ok(resp);
+        }
+    }
+}
---
+++ DotNetTestSite/Controllers/BlogController.cs
@@ -0,0 +1,40 @@
+﻿using DotNetTestSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+
+namespace DotNetTestSite.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<BlogPost> blogPosts = new List<BlogPost>();
+
+            using (var client = new HttpClient())
+            {
+                //HTTP GET
+                client.BaseAddress = new Uri("https://localhost:44309/");
+                var responseTask = client.GetAsync("blog/all");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsStringAsync();
+                    readTask.Wait();
+
+                    var readTaskResult = readTask.Result;
+                    blogPosts.AddRange(Newtonsoft.Json.JsonConvert.DeserializeObject<List<BlogPost>>(readTaskResult));
+                }
+            }
+            return View(blogPosts);
+        }
+        private void GetBlogPosts()
+        {
+            
+        }
+    }
+}
