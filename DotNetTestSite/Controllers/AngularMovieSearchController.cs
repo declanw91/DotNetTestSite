40a39765c114652a63d4cea3c843ffbd313bdc0f
Declan <dwright1991@gmail.com>
2020-11-29 20:01:17 +0000

    Added angular movie search project

---
+++ DotNetTestSite/Controllers/AngularMovieSearchController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetTestSite.Controllers
+{
+    public class AngularMovieSearchController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult MovieResults()
+        {
+            return PartialView();
+        }
+    }
+}
