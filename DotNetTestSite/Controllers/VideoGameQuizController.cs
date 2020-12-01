e19f6d70a1406ab13cc5f987da9cb2a849ecc786
Declan <dwright1991@gmail.com>
2020-12-01 20:38:45 +0000

    Added video game quiz project

---
+++ DotNetTestSite/Controllers/VideoGameQuizController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetTestSite.Controllers
+{
+    public class VideoGameQuizController : Controller
+    {
+        public IActionResult Index()
+        {
+            return PartialView();
+        }
+    }
+}
