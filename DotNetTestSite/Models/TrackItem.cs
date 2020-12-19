328ee6fa6813a9bee572917ebbd065eb1bb752ce
Declan <dwright1991@gmail.com>
2020-12-19 11:17:58 +0000

    Added start of lastfm api controller

---
+++ DotNetTestSite/Models/TrackItem.cs
@@ -0,0 +1,11 @@
+﻿namespace DotNetTestSite.Models
+{
+    public class TrackItem
+    {
+        public string Image { get; set; }
+        public string Name { get; set; }
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public string PlayCount { get; set; }
+    }
+}
