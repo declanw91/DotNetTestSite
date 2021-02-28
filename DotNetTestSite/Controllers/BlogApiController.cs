60a39cbc6b497a2d20712ce633e31c4c015f25ea
Declan <dwright1991@gmail.com>
2021-02-28 18:08:09 +0000

    Updated example page

--- DotNetTestSite/Controllers/BlogApiController.cs
+++ DotNetTestSite/Controllers/BlogApiController.cs
@@ -1,11 +1,9 @@
 ﻿using DotNetTestSite.Models;
-using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
